cf900ba2d1691c1be8f9ebc3a6974c4e3238e4c5
firhan <firhan.faisal1995@gmail.com>
2019-10-27 07:45:21 +0700

    icon cards wavy progress

--- src/components/pages/components/CardPage.js
+++ src/components/pages/components/CardPage.js
@@ -8,7 +8,7 @@ const CardPage = () => {
     return(
         <div>
             <div className="title">
-                Cards
+                Icon Cards
                 <div className="sub-title">
                     <i className="fa fa-info-circle"></i> squared box with bunch of element inside.
                 </div>
@@ -18,8 +18,37 @@ const CardPage = () => {
                 <Container>
                     <Row>
                         <div className="col-6 col-sm-6 col-md-4 col-lg-3">
-                            <IconCard label="Messages" number={100} icon="fa fa-comments"/>  
-                        </div>                  
+                            <IconCard label="iOS Downloads" isWavy={true} number={5207} icon="fa fa-apple"/>  
+                        </div>  
+                        <div className="col-6 col-sm-6 col-md-4 col-lg-3"> 
+                            <IconCard label="Android Downloads" number={10523} icon="fa fa-android"/> 
+                        </div>
+                        <div className="col-6 col-sm-6 col-md-4 col-lg-3">
+                            <div className="help">
+                                *no icon with wavy style
+                            </div>
+                            <IconCard label="Windows Phone Downloads" isWavy={true} number={602} />
+                        </div>  
+                        <div className="col-6 col-sm-6 col-md-4 col-lg-3"> 
+                            <div className="help">
+                                *no icon with blank style
+                            </div>
+                            <IconCard label="Web Access" number={9020321} /> 
+                        </div>            
+                    </Row>
+                </Container>
+            </div>
+            <div className="title">
+                User Cards
+                <div className="sub-title">
+                    <i className="fa fa-info-circle"></i> squared box with bunch of element inside.
+                </div>
+            </div>
+
+            <div className="body">
+                <Container>
+                    <Row>
+            
                     </Row>
                 </Container>
             </div>
