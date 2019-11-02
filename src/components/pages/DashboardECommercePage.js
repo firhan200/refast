6b3a52e5b2f0ee14880976b2160a3157a0ed3861
firhan <firhan.faisal1995@gmail.com>
2019-11-02 09:41:56 +0700

    another dashboard ecom progress

--- src/components/pages/DashboardECommercePage.js
+++ src/components/pages/DashboardECommercePage.js
@@ -1,14 +1,60 @@
 import React, { useState } from 'react';
-import { Box, BoxTitle, Container, Row, Col, IconCard, ChartLine } from '../styles';
+import { Box, BoxTitle, Container, Row, Col, IconCard, ChartLine, ChartDonut, ChartPie } from '../styles';
 
 const DashboardECommercePage = () => {
+    const favoriteProducts = [
+        {
+            label: 'Laptops',
+            data: 1040,
+            color: "#7757F7"
+        },
+        {
+            label: 'Shoess',
+            data: 674,
+            color: "#6E55CE"
+        },
+        {
+            label: 'Watches',
+            data: 233,
+            color: "#5742A7"
+        }
+    ];
+
+    const orders = [
+        {
+            label : 'Pending',
+            data: 102,
+            color: '#CFCAE4'
+        },
+        {
+            label : 'On Going',
+            data: 671,
+            color: '#898AE9'
+        },
+        {
+            label : 'Success',
+            data: 1050,
+            color: '#7FDDC7'
+        },
+        {
+            label : 'Shipped',
+            data: 96,
+            color: '#997CAD'
+        }
+    ]
+
     const sales = {
         labels : ['Jan', 'Feb', 'Apr', 'May', 'Jun'],
         data: [
             {
-                label: 'Sales',
-                color:'#7757F7',
-                data: [2032, 1007, 456, 4002, 7452]
+                label: '2018',
+                color:'#5742A7',
+                data: [400, 760, 825, 993, 1020]
+            },
+            {
+                label: '2019',
+                color:'#9B89E0',
+                data: [500, 600, 700, 750, 700]
             }
         ]
     };
@@ -19,7 +65,7 @@ const DashboardECommercePage = () => {
             <hr/>
             <Container>
                 <Row>
-                    <Col md={3}>
+                    <Col md={3} className="m-b-20">
                         <IconCard 
                             isWavy={true} 
                             statisticValue={-12} 
@@ -28,7 +74,7 @@ const DashboardECommercePage = () => {
                             label="Orders" 
                             icon="fa fa-shopping-cart"/>
                     </Col>
-                    <Col md={3}>
+                    <Col md={3} className="m-b-20">
                         <IconCard 
                             isWavy={true} 
                             number={12506} 
@@ -37,14 +83,14 @@ const DashboardECommercePage = () => {
                             label="Earnings" 
                             icon="fa fa-dollar"/>
                     </Col>
-                    <Col md={3}>
+                    <Col md={3} className="m-b-20">
                         <IconCard 
                             isWavy={true} 
                             number={203} 
                             label="Tickets" 
                             icon="fa fa-ticket"/>
                     </Col>
-                    <Col md={3}>
+                    <Col md={3} className="m-b-20">
                         <IconCard 
                             isWavy={true} 
                             number={3002} 
@@ -53,9 +99,28 @@ const DashboardECommercePage = () => {
                     </Col>
                 </Row>
                 <Row>
-                    <Box md={6} lg={4}>
-                        <ChartLine title="Sales" items={sales}/>
-                    </Box>
+                    <Col md={6} lg={6}>
+                        <Box lg={12}>
+                            <BoxTitle label="Recent Orders"/>
+                        </Box>
+                    </Col>
+                    <Col md={6} lg={6}>
+                        <Container>
+                            <Row>
+                                <Box md={12} lg={6}>
+                                    <ChartDonut title="Most Favorite Products" legend="bottom" items={favoriteProducts}/>
+                                </Box>
+                                <Box md={12} lg={6}>
+                                    <ChartPie title="Orders" legend="bottom" items={orders}/>
+                                </Box>
+                            </Row>
+                            <Row>
+                                <Box md={12}>
+                                    <ChartLine legend="bottom" title="Sales" items={sales}/>
+                                </Box>
+                            </Row>
+                        </Container>
+                    </Col>
                 </Row>
             </Container>
         </div>
