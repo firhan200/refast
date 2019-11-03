2cdc494f152feb4f03dd637b2367cb83b03e4afd
firhan <firhan.faisal1995@gmail.com>
2019-11-03 18:54:55 +0700

    pricing page table

--- src/components/pages/PricingPage.js
+++ src/components/pages/PricingPage.js
@@ -1,7 +1,55 @@
 import React from 'react';
-import { Breadcrumb, BreadcrumbItem, Container, Row, Box } from '../styles';
+import { Breadcrumb, BreadcrumbItem, Container, Row, Box, Table, BoxTitle, H4, Button } from '../styles';
 
 const PricingPage = () => {
+    const pricings = [
+        {
+            label: '24 Hours Support',
+            premium: true,
+            gold: true,
+            silver: false,
+            bronze: false,
+        },
+        {
+            label: 'Unlimited Bandwidth',
+            premium: true,
+            gold: '500GB',
+            silver: '100GB',
+            bronze: '50GB',
+        },
+        {
+            label: 'Storage',
+            premium: '100GB',
+            gold: '50GB',
+            silver: '25GB',
+            bronze: '10GB',
+        },
+        {
+            label: 'Push Notifications',
+            premium: true,
+            gold: true,
+            silver: true,
+            bronze: true,
+        }
+    ];
+
+    const renderIncluded = (isIncluded) => (
+        <div>
+            {/* check type if boolean */}
+            { (typeof isIncluded === 'boolean') ? (
+                //render using icon
+                isIncluded ? (
+                    <i className="fa fa-check"></i>
+                ) : (
+                    <i className="fa fa-remove"></i>
+                )
+            ) : (
+                //render raw string
+                <span>{ isIncluded }</span>
+            ) }
+        </div>
+    )
+
     return(
         <div>
             <div className="title">
@@ -20,7 +68,7 @@ const PricingPage = () => {
                 <Row>
                     <Box sm={12} md={3} className="pricing">
                         <div className="pricing-icon">
-                            <i className="fa fa-server"></i>
+                            <i className="fa fa-snowflake-o"></i>
                         </div>
                         <div className="pricing-title">
                             PREMIUM PACKAGE
@@ -37,7 +85,7 @@ const PricingPage = () => {
                     </Box>
                     <Box sm={12} md={3} className="pricing">
                         <div className="pricing-icon">
-                            <i className="fa fa-server"></i>
+                            <i className="fa fa-star"></i>
                         </div>
                         <div className="pricing-title">
                             GOLD PACKAGE
@@ -54,7 +102,7 @@ const PricingPage = () => {
                     </Box>
                     <Box sm={12} md={3} className="pricing">
                         <div className="pricing-icon">
-                            <i className="fa fa-server"></i>
+                            <i className="fa fa-star-half-o"></i>
                         </div>
                         <div className="pricing-title">
                             SILVER PACKAGE
@@ -71,7 +119,7 @@ const PricingPage = () => {
                     </Box>
                     <Box sm={12} md={3} className="pricing">
                         <div className="pricing-icon">
-                            <i className="fa fa-server"></i>
+                            <i className="fa fa-star-o"></i>
                         </div>
                         <div className="pricing-title">
                             BRONZE PACKAGE
@@ -85,6 +133,64 @@ const PricingPage = () => {
                         <div className="pricing-cost">
                             $ 99
                         </div>
+                    </Box>
+                </Row>
+            
+                <Row>
+                    <Box md={12}>
+                        <BoxTitle label="Table Pricing" subLabel="Show pricing using responsive table to make user easy to get price information included."/>
+                        <Table isResponsive={true} >
+                            <thead>
+                                <tr align="center">
+                                    <th></th>
+                                    <th>
+                                        <div className="m-2">
+                                            Premium
+                                        </div>
+                                        <Button size="small" label="BUY PACKAGE"/>
+                                    </th>
+                                    <th>
+                                        <div className="m-2">
+                                            Gold
+                                        </div>
+                                        <Button size="small" label="BUY PACKAGE"/>
+                                    </th>
+                                    <th>
+                                        <div className="m-2">
+                                            Silver
+                                        </div>
+                                        <Button size="small" label="BUY PACKAGE"/>
+                                    </th>
+                                    <th>
+                                        <div className="m-2">
+                                            Bronze
+                                        </div>
+                                        <Button size="small" label="BUY PACKAGE"/>
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                { pricings.map(pricing => (
+                                    <tr>
+                                        <td>{ pricing.label }</td>
+                                        <td align="center">{ renderIncluded(pricing.premium) }</td>
+                                        <td align="center">{ renderIncluded(pricing.gold) }</td>
+                                        <td align="center">{ renderIncluded(pricing.silver) }</td>
+                                        <td align="center">{ renderIncluded(pricing.bronze) }</td>
+                                    </tr>
+                                )) }
+                            </tbody>
+                            <tfoot>
+                                <tr align="center">
+                                    <th></th>
+                                    <th>$ 599</th>
+                                    <th>$ 499</th>
+                                    <th>$ 299</th>
+                                    <th>$ 99</th>
+                                </tr>
+                            </tfoot>
+                        </Table>
+                        <H4 align="right">*All package is including free website builder.</H4>
                     </Box>
                 </Row>
             </Container>
--- src/components/styles/Container.js
+++ src/components/styles/Container.js
@@ -1,11 +1,22 @@
 import React from 'react';
 
+//libs
+import PropTypes from 'prop-types'; // prop types
+
 const Container = (props) => {
+    //get custom class
+    const customClassName = typeof props.className !== 'undefined' ? props.className : '';
+
     return(
-        <div className="container custom-container">
+        <div className={"container custom-container " + customClassName}>
             { props.children }
         </div>
     );
+}
+
+//prop types initialize
+Container.propTypes = {
+    className: PropTypes.string
 }
 
 export default Container;
